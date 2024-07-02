69c950b74364a45f07dbb14680749e4ae7585c7d
SAIVARAPRASAD K <saikondaka07@gmail.com>
2024-07-02 21:04:18 +0530

    For Pratice Purpose

---
+++ hello.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        System.out.println("Java Scan the value:" x);
+    }
+}
